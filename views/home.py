0664e3a1caa14c5e6bd12a69080fe9cf57de5e64
Leah Talary <talarlea@students.zhaw.ch>
2026-03-04 10:38:39 +0100

    Nach Übungsstunde

--- views/home.py
+++ views/home.py
@@ -10,7 +10,11 @@ Diese App wurde von folgenden Personen entwickelt:
 - Steffi Neuhof (bierist3@students.zhaw.ch)
 
 
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+Diese App ist das Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+Unser erstes Projekt ist der Notendurchschnittsrechner. Dieser ermöglicht es, den persönlichen Notendurchschnitt einer Modulgruppe zu berechnen.
+Dazu werden für jedes Fach die erzielte Note und die entsprechende ECTS‑Gewichtung eingetragen.
+Aus diesen Angaben wird automatisch der gewichtete Durchschnitt ermittelt, sodass auf einen Blick ersichtlich ist, ob die Modulgruppe bestanden wurde oder nicht.
 
 Autoren: Steffi Neuhof (bierist3@students.zhaw.ch), Leah Talary (talarlea@students.zhaw.ch)
 """
