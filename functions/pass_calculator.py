851c67e8804a0da5976cf2654c1718ce858b38a7
Leah Talary <talarlea@students.zhaw.ch>
2026-03-11 10:30:28 +0100

    changes

--- functions/pass_calculator.py
+++ functions/pass_calculator.py
@@ -15,36 +15,3 @@ def ist_bestanden(durchschnitt, grenze=4.0):
     return durchschnitt >= grenze
 
 
-def zeige_statistik(df):
-    if len(df) == 0:
-        return
-
-    beste_note = df["Note"].max()
-    schlechteste_note = df["Note"].min()
-    anzahl_faecher = len(df)
-
-    st.subheader("Statistik")
-    st.write(f"Anzahl Fächer: {anzahl_faecher}")
-    st.write(f"Beste Note: {beste_note}")
-    st.write(f"Schlechteste Note: {schlechteste_note}")
-
-def download_csv(df):
-    csv = df.to_csv(index=False)
-
-    st.download_button(
-        label="CSV herunterladen",
-        data=csv,
-        file_name="noten_ects.csv",
-        mime="text/csv"
-    )
-
-def validiere_daten(df):
-    if df["ECTS"].sum() == 0:
-        st.warning("Die ECTS dürfen nicht 0 sein.")
-        return False
-
-    if df["Note"].isnull().any():
-        st.warning("Bitte alle Noten ausfüllen.")
-        return False
-
-    return True
--- views/Did_I_Pass_Calculator.py
+++ views/Did_I_Pass_Calculator.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import pandas as pd
-
-from views.functions_Pass import (
-    create_default_dataframe,
+from functions.pass_calculator import (
     berechne_durchschnitt,
-    zeige_ergebnis,
-    zeige_status,
-    zeige_statistik,
-    download_csv,
-    validiere_daten
-)
+    ist_bestanden,)
 
 def create_default_dataframe():
     return pd.DataFrame({
@@ -91,3 +84,37 @@ if submitted:
 
         if durchschnitt < 4.0:
             exmatrikulation_dialog()
+
+def zeige_statistik(df):
+    if len(df) == 0:
+        return
+
+    beste_note = df["Note"].max()
+    schlechteste_note = df["Note"].min()
+    anzahl_faecher = len(df)
+
+    st.subheader("Statistik")
+    st.write(f"Anzahl Fächer: {anzahl_faecher}")
+    st.write(f"Beste Note: {beste_note}")
+    st.write(f"Schlechteste Note: {schlechteste_note}")
+
+def download_csv(df):
+    csv = df.to_csv(index=False)
+
+    st.download_button(
+        label="CSV herunterladen",
+        data=csv,
+        file_name="noten_ects.csv",
+        mime="text/csv"
+    )
+
+def validiere_daten(df):
+    if df["ECTS"].sum() == 0:
+        st.warning("Die ECTS dürfen nicht 0 sein.")
+        return False
+
+    if df["Note"].isnull().any():
+        st.warning("Bitte alle Noten ausfüllen.")
+        return False
+
+    return True
