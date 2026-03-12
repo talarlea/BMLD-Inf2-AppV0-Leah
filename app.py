eca88aed458387d14d9b691d7a9b22a3a6ce1057
steffinitely <bierist3@students.zhaw.ch>
2026-03-12 18:43:10 +0100

    Vorlesung 4 sachen

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import streamlit as st
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+data_manager = DataManager(
+    fs_protocol='webdav',
+    fs_root_folder='bmld_app'
+)
+login_manager = LoginManager(data_manager)
+login_manager.login_register()
 
 if 'data_df' not in st.session_state:
-    st.session_state['data_df'] = pd.DataFrame()
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',
+        initial_value=pd.DataFrame(),
+        parse_dates=['timestamp']
+    )
 
     
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
@@ -12,3 +25,4 @@ pg_second = st.Page("views/Did_I_Pass_Calculator.py", title="Did I pass? - Calcu
 
 pg = st.navigation([pg_home, pg_second])
 pg.run()
+
